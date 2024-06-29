4401f3556a274c4845340d4cf3aee12df0a9b14b
Guilherme Hoerning Reinert <guilhermeh.rei@gmail.com>
2024-06-29 19:58:14 -0300

    finished the second work about binary trees on Haskell

--- haskell/PFN/trabalho2/trabalhoTree.hs
+++ haskell/PFN/trabalho2/trabalhoTree.hs
@@ -5,22 +5,16 @@
 -- índice de linhas em que as palavras ocorrem nesse documento, as informações (palavras e lista de
 -- linhas em que ocorrem) devem ser armazenada em uma árvore binária de pesquisa declarada como:
 
+import Data.List (sort)
+
 type Doc = String
 type Line = String
 type Word' = String
 type Index = Int
 
-lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\nExcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
-
-data Erdtree a = No a (Erdtree a) (Erdtree a) | Leaf
-    deriving Show
-
 -- A impressão das palavras e índices deve ser feita percorrendo a árvore em ordem.
-
--- data Tree = Node Word' [Int] Tree Tree | Leaf deriving Show
--- makeIndexTree :: Doc -> Tree
-
-
+data Tree = Node Word' [Index] Tree Tree | Leaf
+    deriving Show
 
 -- a) Separar o documento em linhas: lines :: Doc → [Line]
 
@@ -28,28 +22,24 @@ data Erdtree a = No a (Erdtree a) (Erdtree a) | Leaf
 
 -- b) Numerar as linhas do documento: numLines :: [Line] → [(Int,Line)]
 
-numLines :: Doc -> [(Int, Line)]
+numLines :: Doc -> [(Index, Line)]
 numLines xs = numLines' 1 (lines xs)
 
-numLines' :: Int -> [Line] -> [(Int, Line)]
+numLines' :: Index -> [Line] -> [(Index, Line)]
 numLines' _ [] = []
 numLines' n (x:xs) = (n,x):numLines' (n+1) xs
-
-isThere :: a -> [a]-> Bool
-isThere y [] = False
-isThere y (x:xs) = if y == x then True else isThere y xs
 
 -- c) Associar a cada ocorrência de uma palavra do documento o número da linha em que essa
 -- palavra ocorre: allNumWords :: [(Int,Line)] → [(Int,Word’)]
 
-allNumWords :: [(Int, Line)] -> [(Int, Word')]
+allNumWords :: [(Index, Line)] -> [(Index, Word')]
 allNumWords ys = allNumWords' (worderize ys)
 
-worderize :: [(Int, Line)] -> [(Int, [Line])]
+worderize :: [(Index, Line)] -> [(Index, [Line])]
 worderize [] = []
 worderize ((n,x):xs) = ((n,words x):worderize xs)
 
-allNumWords' :: [(Int, [Line])] -> [(Int, Word')]
+allNumWords' :: [(Index, [Line])] -> [(Index, Word')]
 allNumWords' ((_,[]):ys) = allNumWords' ys 
 allNumWords' [] = []
 allNumWords' ((n,(x:xs)):ys) = (n,x):allNumWords' ((n,xs):ys)
@@ -57,16 +47,60 @@ allNumWords' ((n,(x:xs)):ys) = (n,x):allNumWords' ((n,xs):ys)
 -- d) Inserir elementos em uma lista ordenada, o elemento deve ser inserido em uma posição que
 -- mantenha a lista resultante ordenada. Caso a lista já contenha o elemento não deve ocorrer a
 -- inserção:
--- insOrd :: a → [a] → [a]
 
+insOrd :: Index -> [Index] -> [Index]
+insOrd x ys
+    | not (isThere x ys) = insOrd' x ys
+    | otherwise = ys
 
+insOrd' :: Index -> [Index] -> [Index]
+insOrd' x [] = [x]
+insOrd' x (y:ys)
+    | x < y = x:(y:ys)
+    | otherwise = y:insOrd' x ys
+
+isThere :: Index -> [Index] -> Bool
+isThere x [] = False
+isThere x (y:ys)
+    | x == y = True
+    | otherwise = isThere x ys
 
 -- e) Inserir uma palavra e linha de ocorrência na árvore (Tree), caso a palavra já tenha sido
 -- inserida apenas a linha deve ser adicionada a lista de linhas relacionadas com a palavra.
 -- Deve ser usada a função definida no item anterior para essa tarefa:
 -- ins :: Word’ → Int → Tree → Tree
 
-
+inserTree :: (Index, Word') -> Tree -> Tree
+inserTree (is, w) Leaf = Node w [is] Leaf Leaf
+inserTree (is, w) (Node wt it left right)
+    | w == wt = Node w (insOrd is it) left right
+    | w < wt = Node wt it (inserTree (is,w) left) right
+    | w > wt = Node wt it left (inserTree (is,w) right)
 
 -- f) Percorrer a lista com as duplas de palavras e linhas inserido cada uma delas na árvore:
--- mIndexTree :: [(Int,Word’)] →Tree
+-- mIndexTree :: [(Int,Word’)] → Tree
+
+mIndexTree :: [(Index, Word')] -> Tree
+mIndexTree [] = Leaf
+mIndexTree (x:xs) = (inserTree x (mIndexTree xs))
+
+makeIndexTree :: Doc -> Tree
+makeIndexTree doc = mIndexTree(allNumWords(numLines doc))
+
+-- Texto embutido de teste
+lorem = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\nExcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum"
+tesTree = makeIndexTree lorem
+
+-- Ler o arquivo e plotar a árvore
+main = do putStr "Arquivo: "
+          file_name <- getLine
+          txt <- readFile file_name
+          let document = makeIndexTree txt
+          print document
+          ploTree document
+
+ploTree :: Tree -> IO()
+ploTree Leaf = return ()
+ploTree (Node w it left right) = do putStr (w++": "++show it++"\n")
+                                    ploTree left
+                                    ploTree right

36ea310d493771894489f92594ca6d975d452ec4
Guilherme Hoerning Reinert <guilhermeh.rei@gmail.com>
2024-07-04 10:21:07 -0300

    practiced data tree before the PFN test on today afternoon

---
+++ c/petvet/petRasc.c
@@ -0,0 +1,185 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+// #include <windows.h>
+
+float matriz[4][99]; // Matriz de preços pagos pelo cliente
+// [0]: Vacinas
+// [1]: Castrações
+// [2]: Rações
+// [3]: Medicamentos
+
+float somat(int pos){
+    float soma = 0;
+    for(int l=0; l<4; l++){
+        for(int c=0; c<(pos+1); c++){
+            soma += matriz[l][c];
+        }
+    }
+    return soma;
+}
+
+int main () {
+    //SetConsoleOutputCP(CP_UTF8);
+
+    char nome[60]="0", nomeani[60], data[30], remedio[99], telefone[25], ddd[10],
+    caso, animal, tamanho, resposta;
+    int pacotes, vez=-1;
+    double total, valorRac;
+
+    setlocale(LC_ALL, "pt_BR.UTF-8");
+
+    printf("\n──────▄▀▄─────▄▀▄\n─────▄█░░▀▀▀▀▀░░█▄\n─▄▄──█░░░░░░░░░░░█──▄▄\n█▄▄█─█░░▀░░┬░░▀░░█─█▄▄█\n");
+ 
+    while((resposta!='n')&&(resposta!='N')){
+        vez++;
+        printf("\n--------------------------------------------\n");
+        printf("• Olá! Bem vindo(a) ao nosso Petshop PetVet!\n");
+        printf("Nossa clínica oferece os seguintes serviços:\n");
+        printf("1) Vacina\n");
+        printf("2) Castração\n");
+        printf("3) Venda de ração\n");
+        printf("4) Medicamentos\n");
+        if(strcmp(nome, "0") == 0){ // GAMBIARRA. cada vez que o loop do while era executado, <nome> era ignorado
+            printf("• Qual seu o nome social? ");
+            scanf(" %[^\n]s", nome);
+        }
+        printf("• Escolha um de nossos serviços enumerados acima:\n >> ");
+        scanf(" %c", &caso);
+        getchar();
+
+        switch (caso) {
+            case '1': // VACINA
+                printf("• Felino ou Canino? (F/C) \n >> ");
+                scanf(" %c", &animal);
+                getchar();
+                if (animal == 'f' || animal == 'F') {
+                    printf("• Nome do felino: \n >> ");
+                    scanf(" %[^\n]s", nomeani);
+                    getchar();
+                } else if (animal == 'c' || animal == 'C') {
+                    printf("• Nome do canino: \n >> ");
+                    scanf(" %[^\n]s", nomeani);
+                    getchar();
+                } else {
+                    printf("Digite uma opção válida!\n");
+                    return 0;
+                }
+                printf("• Data da vacinação: \n >> ");
+                scanf(" %[^\n]s", data);
+                getchar();
+
+                printf("-----------------------\n");
+                printf("Cliente: %s\n", nome);
+                printf("Serviço: Vacina\n");
+                if (animal == 'F' || animal == 'f') {
+                    printf("Felino: %s\n", nomeani);
+                } else {
+                    printf("Canino: %s\n", nomeani);
+                }
+                printf("Data: %s\n", data);
+                printf(" >> Preço total: R$ 190,00\n");
+                matriz[0][vez] = 190.0;
+                break;
+
+            case '2': // CASTRAÇÃO
+                printf("• Felino ou Canino? (F/C) \n >> ");
+                scanf(" %c", &animal);
+                getchar();
+                if (animal == 'f' || animal == 'F') {
+                    printf("• Nome do felino: \n >> ");
+                    scanf(" %[^\n]s", nomeani);
+                    getchar();
+                } else if (animal == 'c' || animal == 'C') {
+                    printf("• Nome do canino: \n >> ");
+                    scanf(" %[^\n]s", nomeani);
+                    getchar();
+                } else {
+                    printf("Digite uma opção válida!\n");
+                    return 0;
+                }
+                printf("• Data da castração: \n >> ");
+                scanf(" %[^\n]s", data);
+                getchar();
+                printf("-----------------------\n");
+
+                printf("Cliente: %s\n", nome);
+                printf("Serviço: Castração\n");
+                if (animal == 'F' || animal == 'f') {
+                    printf("Felino: %s\n", nomeani);
+                    printf("Data: %s\n", data);
+                    printf(" >> Preço total: R$ 220,00\n");
+                    matriz[1][vez] = 220.0;
+                } else {
+                    printf("Canino: %s\n", nomeani);
+                    printf("Data: %s\n", data);
+                    printf(" >> Preço total: R$ 280,00\n");
+                    matriz[1][vez] = 280.0;
+                }
+                
+            break;
+
+            case '3': // VENDA DE RAÇÃO
+                printf("• Quantos pacotes serão comprados? \n >> ");
+                scanf("%d", &pacotes);
+                getchar();
+                printf("• Qual será o tamanho dos pacotes? (P/M/G) \n >> ");
+                scanf(" %c", &tamanho);
+                getchar();
+                if (tamanho == 'p' || tamanho == 'P') {
+                    valorRac = 30 * pacotes;
+                } else if (tamanho == 'm' || tamanho == 'M') {
+                    valorRac = 70 * pacotes;
+                } else if (tamanho == 'g' || tamanho == 'G') {
+                    valorRac = 120 * pacotes;
+                } else {
+                    printf("Digite uma opção válida!\n");
+                return 0;
+                }
+
+                printf("-----------------------\n");
+
+                printf("Cliente: %s\n", nome);
+                printf("Serviço: Venda de ração\n");
+                printf("Pedido: %d unidades de tamanho %c cada.\n", pacotes, tamanho);
+                if (pacotes > 1) {
+                    printf("Valor bruto: R$ %.2lf\n", valorRac);
+                    printf("Desconto: 10 %% \n");
+                    total = 0.9 * valorRac;
+                } else {
+                    printf("Desconto: 0 %% \n");
+                    total = valorRac;
+                }
+                printf(" >> Preço total: R$ %.2lf\n", total);
+                matriz[2][vez] = total;
+            break;
+
+            case '4': // MEDICAMENTOS
+                printf("• Digite o nome do remédio para encomendar:\n >> ");
+                scanf(" %[^\n]s", remedio);
+                getchar();
+
+                printf("• Digite o seu telefone, com espaço após DDD:\n >> ");
+                scanf(" %s %s", ddd, telefone);
+                getchar();
+
+                printf("-----------------------\n");
+
+                printf("Cliente: %s\n", nome);
+                printf("Serviço: Encomenda de medicamento\n");
+                printf("Medicamento: %s\n", remedio);
+                printf("Contato: %s %s\n", ddd, telefone);
+            break;
+
+        default:
+            printf("Digite uma opção válida!\n");
+            return 0;
+    }
+    printf("\nDeseja pedir outro serviço? (S/N) ");
+    scanf("%c", &resposta);
+    }
+    printf("\nPreço total da(s) %d chamada(s) do PetVet!\n >> R$ %2.f\n", vez+1, somat(vez));
+
+return 0;
+}
--- haskell/PFN/aulas/tree/TREE.hs
+++ haskell/PFN/aulas/tree/TREE.hs
@@ -1,22 +1,22 @@
-data Mordrassil tear = No tear (Mordrassil tear) (Mordrassil tear) | Leaf
+data Mordrassil a = Node a (Mordrassil a) (Mordrassil a) | Leaf
     deriving (Show, Ord, Eq)
 
 -- creates a tree with a single node
 singleton :: a -> Mordrassil a
-singleton x = No x Leaf Leaf
+singleton x = Node x Leaf Leaf
 
 -- insert a element into a tree
 treeInsert :: (Ord e) => e -> Mordrassil e -> Mordrassil e
 treeInsert x Leaf = singleton x
-treeInsert x (No a left right) 
-    | x == a = No x left right -- returns the same tree if the root elemente is the same as the new one
-    | x < a  = No a (treeInsert x left) right -- insert the right element
-    | x > a  = No a left (treeInsert x right) -- insert the left element
+treeInsert x (Node a left right) 
+    | x == a = Node x left right -- returns the same tree if the root elemente is the same as the new one
+    | x < a  = Node a (treeInsert x left) right -- insert the right element
+    | x > a  = Node a left (treeInsert x right) -- insert the left element
 
  -- find a element in the tree
 treeElem :: (Ord e) => e -> Mordrassil e -> Bool
 treeElem x Leaf = False -- no element on an empty tree
-treeElem x (No a left right)
+treeElem x (Node a left right)
     | x == a = True -- found the element
     | x < a  = treeElem x left -- know it is lower than the root element
     | x > a  = treeElem x right -- know it is higher than the root element
@@ -28,4 +28,14 @@ treeList (x:xs) = treeInsert x (treeList xs)
 
 tree2List :: (Ord e) => Mordrassil e -> [e]
 tree2List Leaf = []
-tree2List (No e left right) = tree2List left ++ [e] ++ tree2List right
+tree2List (Node e left right) = tree2List left ++ [e] ++ tree2List right
+
+-- return the biggest value on the tree
+bigTree :: (Num e) => (Ord e) => Mordrassil e -> e
+bigTree tree = bigTree' tree 0
+
+bigTree' :: (Eq e) => (Ord e) => Mordrassil e -> e -> e
+bigTree' Leaf x = x 
+bigTree' (Node a left right) x
+    | a <= x = bigTree' right x
+    | a > x = bigTree' right a

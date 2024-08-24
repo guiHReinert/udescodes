067a7399d48de72843f9a3a8d1e329dda95db5e6
Guilherme Hoerning Reinert <guilhermeh.rei@gmail.com>
2024-08-24 13:37:54 -0300

    array sorting algorithm done

--- c/LPG/listaLPG1.c
+++ c/LPG/listaLPG1.c
@@ -42,8 +42,6 @@ void quadrante(){
 }
 //Questão 2
 
-
-
 void triangulo(){
     float lados[3];
     for(int i=0; i<3; i++){
--- c/LPG/sort.c
+++ c/LPG/sort.c
@@ -3,16 +3,22 @@
 void main(){
     int array[9] = {9,1,8,2,7,3,6,4,5};
 
-    for(int i=0; i<(sizeof(array)/sizeof(array[0])-1); i++){
+    for(int c=0; c<9; c++){
+        printf("%d ", array[c]);
+    }
+    for(int i=0; i<(sizeof(array)/sizeof(array[0])); i++){
         int min = i;
-        for(int s=i+1; s<(sizeof(array)/sizeof(array[0])-1); s++){
+        for(int s=i+1; s<(sizeof(array)/sizeof(array[0])); s++){
             if(array[min]>array[s]){
                 min = s;
                 }
-            
             }
         int temp = array[i];
         array[i] = array[min];
         array[min] = temp;
         }
+    printf("\n");
+    for(int c=0; c<9; c++){
+        printf("%d ", array[c]);
+    }
     }

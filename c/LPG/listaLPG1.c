a1c96d43892f6557f5cc0bb51d33b7a74cca50b5
Guilherme Hoerning Reinert <guilhermeh.rei@gmail.com>
2024-08-25 19:06:28 -0300

    finished the question 10 on listaLPG1.c

--- c/LPG/listaLPG1.c
+++ c/LPG/listaLPG1.c
@@ -1,14 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
+
+// Funções auxiliares
+float* arrSort(float* array, int arr_size){ // The array and size parameters must be diffent
+    for(int i=0; i<arr_size; i++){
+        int min = i;
+        for(int s=i+1; s<arr_size; s++){
+            if(array[min]>array[s]){
+                min = s;
+                }
+            }
+        float temp = array[i];
+        array[i] = array[min];
+        array[min] = temp;
+        }
+    return array;
+}
 
 // Questão 1
 void quadrante(){
-    float x, y;
+    float coord[2];
     char quad[9];
-
     printf("Digite dois valores para x e y: \n");
-    scanf("%f %f", &x, &y);
+    for(int c=0; c<2; c++){
+        scanf("%f", &coord[c]);
+    }
+    float x = coord[0], y = coord[1];
     if(x==0){
         if(y==0){
             strcpy(quad, "Origem");
@@ -41,31 +60,181 @@ void quadrante(){
     printf("%s\n", quad);
 }
 //Questão 2
-
 void triangulo(){
     float lados[3];
     for(int i=0; i<3; i++){
-        int num = 0;
+        float num = 0;
         scanf("%f", &num);
         lados[i] = num;
     };
-    
-    if(a==b==c){
-        printf("TRIANGULO EQUILATERO \n");
+    arrSort(lados, sizeof(lados)/sizeof(lados[0]));
+    int a=lados[2], b=lados[1], c=lados[0];
+    if(a>=b+c){
+        printf("NAO FORMA TRIANGULO\n");
     }
     else{
-        if(((a==b)||(b==c)||(a==c))){
-            printf("TRIANGULO ISOSCELES");
+        if(a*a==b*b+c*c){
+            printf("TRIANGULO RETANGULO\n");
         }
-        else{}
+        else if(a*a>b*b+c*c){
+            printf("TRIANGULO OBTUSANGULO\n");
+        }
+        else if(a*a<b*b+c*c){
+            printf("TRIANGULO ACUTANGULO\n");
+        }
+        if((a==b)&&(b==c)){
+            printf("TRIANGULO EQUILATERO\n");
+        }
+        else if(((a==b)||(b==c)||(a==c))){
+                printf("TRIANGULO ISOSCELES\n");
+            }
     }
+}
+// Questão 3
+void tempoJogo(){
+    int tempos[2], durac;
+    for(int i=0; i<2; i++){
+            scanf("%d", &tempos[i]);
+    }
+    if(tempos[0]<24 && tempos[1]<24){
+        durac = tempos[1]-tempos[0];
+        if(tempos[0]==0 && tempos[1]==0){
+            durac = 24;
+        }
+        else if(tempos[0]>tempos[1]){
+            durac += 24;
+        }
+        printf("O JOGO DUROU %d HORAS(S)\n", durac);
+    }
+    else{
+        printf("Digite os horarios certo!\n");
+    }
+}
+// Questão 4
+void PIPN(){
+    int qntNum=0, cPar=0, cImp=0, cPos=0, cNeg=0;
+    scanf("%d", &qntNum);
+    int nums[qntNum];
+    for(int c=0; c<qntNum; c++){
+        scanf("%d", &nums[c]);
+        if(nums[c]%2==0){
+            cPar += 1;
+        }
+        else{
+            cImp += 1;
+        }
+        if(nums[c]>0){
+            cPos += 1;
+        }
+        else if(nums[c]<0){
+            cNeg += 1;
+        }
+    }
+    printf("%d valor(es) par(es) \
+    \n%d valor(es) impar(es) \
+    \n%d valor(es) positivo(s) \
+    \n%d valor(es) negativo(s)\n", cPar, cImp, cPos, cNeg);
+}
+// Questão 5
+void mediaPond(){
+    int qntMed;
+    scanf("%d", &qntMed);
+    float notas[qntMed][3], medias[qntMed];
+    for(int g=0; g<qntMed; g++){
+        for(int u=0; u<3; u++){
+            scanf("%f", &notas[g][u]);
+        }
+        medias[g] = (2*notas[g][0] + 3*notas[g][1] + 5*notas[g][2])/10;
+    }
+    for(int i=0; i<qntMed; i++){
+        printf("%.1f\n", medias[i]);
+    }
+}
+// Questão 6
+void somaImparesCon_1(){
+    float range0[2];
+    int range[2], soma=0;
+    for(int g=0; g<2; g++){
+        scanf("%f", &range0[g]);
+    }
+    range[0] = (int)arrSort(range0, sizeof(range0)/sizeof(range0[0]))[0];
+    range[1] = (int)arrSort(range0, sizeof(range0)/sizeof(range0[0]))[1];
+    for(int u=range[0]+1; u<range[1]; u++){
+        if(u%2!=0){
+            soma += u;
+        }
+    }
+    printf("%d\n", soma);
+}
+// Questão 7
+void somaImparesCon_2(){
+    int qntTestes=0;
+    scanf("%d", &qntTestes);
+    int somas[qntTestes];
+    for(int g=0; g<qntTestes; g++){
+        float range0[2];
+        int range[2], soma=0;
+        for(int u=0; u<2; u++){
+            scanf("%f", &range0[u]);
+        }
+        range[0] = (int)arrSort(range0, sizeof(range0)/sizeof(range0[0]))[0];
+        range[1] = (int)arrSort(range0, sizeof(range0)/sizeof(range0[0]))[1];
+        for(int i=range[0]+1; i<range[1]; i++){
+            if(i%2!=0){
+                soma += i;
+            }
+        }
+        somas[g] = soma;
+    }
+    printf("\n");
+    for(int c=0; c<qntTestes; c++){
+        printf("%d\n", somas[c]);
+    }
+}
+// Questão 8
+void serieHarmonica(){
+    unsigned int terms=0;
+    float soma=0;
+    scanf("%d", &terms);
+    for(int g=1; g<=terms; g++){
+        soma += 1.0/g;
+    }
+    printf("\n%f\n", soma);
+}
+// Questão 9
+void logNeperianoB2(){
+    unsigned int terms=0;
+    float soma=0;
+    scanf("%d", &terms);
+    for(int g=1; g<=terms; g++){
+        soma += pow(-1, g+1)/(float)g;
+    }
+    printf("\n%lf\n", soma);
+}
+//Questão 10
+float regressao(int k, float div, int coef){ 
+    if(k>0){
+        return coef*(4/div) + regressao(k-1, div+2, -coef);
+    }
+    else{
+        return 0;
+    }
+}
+void serieGregLeib(){
+    unsigned int terms=0;
+    scanf("%d", &terms);
+    printf("\n%f\n", regressao(terms, 1, 1));
+}
+// Questão 11
+void serieNilak(){
+    
 }
 
 void main(){
     unsigned int ans;
 
     printf("Escolha uma questão das listas: \
-    \n >> Lista 1 - Selecao e Repeticao: \
+    \n >> Lista 1.1 - Selecao e Repeticao: \
     \n1) Coordenadas de um ponto \
     \n2) Tipos de Triangulos \
     \n3) Tempo de Jogo \
@@ -73,7 +242,7 @@ void main(){
     \n5) Medias Ponderadas \
     \n6) Soma de Impares Consecutivos I \
     \n7) Soma de Impares Consecutivos II \
-    \n\n >> Lista 2 - Geracao de Series: \
+    \n\n >> Lista 1.2 - Geracao de Series: \
     \n(1) 8) Serie Harmonica \
     \n(2) 9) Logaritmo Natural de 2 \
     \n(3a) 10) Serie de Gregory-Leibniz \
@@ -88,7 +257,14 @@ void main(){
     switch(ans){
         case 1: quadrante(); break;
         case 2: triangulo(); break;
-        default: printf("Digite um índice correto!\n");
+        case 3: tempoJogo(); break;
+        case 4: PIPN(); break;
+        case 5: mediaPond(); break;
+        case 6: somaImparesCon_1(); break;
+        case 7: somaImparesCon_2(); break;
+        case 8: serieHarmonica(); break;
+        case 9: logNeperianoB2(); break;
+        case 10: serieGregLeib(); break;
+        default: printf("Digite um índice correto! (entre 1 e 16)\n");
     }
-
 }
--- c/LPG/sort.c
+++ c/LPG/sort.c
@@ -1,24 +1,32 @@
 #include <stdio.h>
 
-void main(){
-    int array[9] = {9,1,8,2,7,3,6,4,5};
-
-    for(int c=0; c<9; c++){
-        printf("%d ", array[c]);
+float* arrSort(float* array, int arr_size){ // The array and size parameters must be diffent
+    for(int c=0; c<arr_size; c++){ // Printing unchanged array
+        printf("%f ", array[c]);
     }
-    for(int i=0; i<(sizeof(array)/sizeof(array[0])); i++){
+    for(int i=0; i<arr_size; i++){
         int min = i;
-        for(int s=i+1; s<(sizeof(array)/sizeof(array[0])); s++){
+        for(int s=i+1; s<arr_size; s++){
             if(array[min]>array[s]){
                 min = s;
                 }
             }
-        int temp = array[i];
+        float temp = array[i];
         array[i] = array[min];
         array[min] = temp;
         }
     printf("\n");
-    for(int c=0; c<9; c++){
-        printf("%d ", array[c]);
+    for(int c=0; c<arr_size; c++){ // Printing changed array
+        printf("%f ", array[c]);
     }
+    printf("\n");
+    return array;
+}
+
+void main(){
+    int arr1[] = {9,1,8,2,7,3,6,4,5};
+    int* arr2 = (int*)arrSort((float*)arr1, sizeof(arr1)/sizeof(arr1[0]));
+    for(int g=0; g<9; g++){
+        printf("%d ", arr2[g]);
     }
+}

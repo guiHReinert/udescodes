00e7f5af86063447a6cfff502d0db0f79acbfa54
Guilherme Hoerning Reinert <guilhermeh.rei@gmail.com>
2024-09-11 21:40:58 -0300

    continued the listaLPG2.c file

--- c/LPG/listaLPG2.c
+++ c/LPG/listaLPG2.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Funções auxiliares
+float* arrSort(float* array, int arr_size){ // The array and size parameters must be diffent
+    for(int i=0; i<arr_size; i++){
+        int min = i;
+        for(int s=i+1; s<arr_size; s++){
+            if(array[min]>array[s]){
+                min = s;
+                }
+            }
+        float temp = array[i];
+        array[i] = array[min];
+        array[min] = temp;
+        }
+    return array;
+}
+
 /* 1) Faça um programa que lê os três lados de um triângulo e determina o seu tipo, conforme
 códigos a seguir. Os códigos devem ser retornados por uma função de tipo int, que recebe
 os lados do triângulo como parâmetro. Protótipo da função:
@@ -12,8 +28,44 @@ ao outro lado);
 2. Triângulo isóceles;
 3. Triângulo escaleno.
 */
-void tipoTriangulo(){
-
+int tipo_triangulo(float x, float y, float z){
+    // printf("%d %d %d\n", x+y<=z, y+z<=x, x+z<=y);
+    if((x+y<=z)||(y+z<=x)||(x+z<=y)){
+        return 0;
+    }
+    else{
+        if((x==y)&&(y==z)){
+            return 1;
+        }
+        else if((x==y)||(y==z)||(x==z)){
+            return 2;
+        }
+        else{
+            return 3;
+        }
+    }
+}
+void triangleType(){
+    int ans;
+    float lados[3]; 
+    for(int c=0; c<3; c++){
+        scanf("%f", &lados[c]);
+    }
+    ans = tipo_triangulo(lados[0], lados[1], lados[2]);
+    switch(ans){
+        case 0: 
+            printf("Nao forma triangulo.\n");
+            break;
+        case 1:
+            printf("Triangulo equilatero.\n");
+            break;
+        case 2: 
+            printf("Triangulo isosceles.\n");
+            break;
+        case 3:
+            printf("Triangulo escaleno.\n");
+            break;
+    }
 }
 
 /* 2) Faça uma função que recebe três valores inteiros e retorna o maior valor. É preciso
@@ -22,11 +74,41 @@ maior valor. Por exemplo, os parâmetros poderiam ser 5, 8 e 8. Neste caso, a fu
 retornar 8.
 */
 
+int maior_valor(int n1, int n2, int n3){
+    if(n1>=n2&&n1>=n3){
+        return n1;
+    }
+    else if(n2>=n1&&n2>=n3){
+        return n2;
+    }
+    else if(n3>=n1&&n3>=n2){
+        return n3;
+    }
+}
+void biggestNum(){
+    int num, nums[3];
+    for(int c=0; c<3; c++){
+        scanf("%d", &nums[c]);
+    }
+    num = maior_valor(nums[0], nums[1], nums[2]);
+    printf("%d\n", num);
+}
+
 /* 3) Escreva um programa que informa se um caractere digitado pelo usuário representa um
 dígito de 0 a 9. A verificação deve ser feita por uma função booleana (int) que recebe um
 char como parâmetro. Caso o caractere seja um dígito, converta-o para um valor inteiro e
 o armazene em uma variável int. Em seguida, mostre o valor inteiro na tela.
 */
+int verif_inteiro(){
+    
+}
+void printInt(){
+    char carac;
+    int ans;
+    scanf("%c", &carac);
+    ans = verif_inteiro(carac);
+    printf("%d\n", ans);
+}
 
 /* 4) Faça uma função que recebe 2 parâmetros, x e y, e calcule a soma dos números impares
 entre eles (sem contar com eles mesmos). Repare que a função deve levar em conta de
@@ -84,7 +166,9 @@ void main(){
     \n\n > ");
     int ans; scanf("%d", &ans);
     switch(ans){
-        case 1: tipoTriangulo(); break;
+        case 1: triangleType(); break;
+        case 2: biggestNum(); break;
+        case 3: printInt(); break;
         default: printf("Digite um índice correto! (entre 1 e 10)\n");
     }
 }

385e0a4be371e1791dc2bc57cf2a1bf735aff67f
Guilherme Hoerning Reinert <guilhermeh.rei@gmail.com>
2024-12-04 00:16:09 -0300

    trabalho2.c: solved(??) the realloc issue on struct musica

--- c/LPG/trabalho2/trabalho2.c
+++ c/LPG/trabalho2/trabalho2.c
@@ -19,9 +19,10 @@ struct musica{
      struct Compositor compositor;
 };
 
-void printm(); // printa o menu de opcoes
-void inserir(struct musica *cd, int *n, int *m);
-void print(struct musica *mus, int *id);
+void printMenu(); // printa o menu de opcoes
+void inserir(struct musica **cd, int *n);
+void printar(struct musica mus);
+int search(struct musica *cd, char *key, int *len);
 
 int main() {
      struct musica *cd;
@@ -29,67 +30,96 @@ int main() {
      int n = 0, m = 0;
      int t = 0;
      while(t != 5){ 
-          printm(); 
+          printMenu(); 
           scanf("%d", &t);
           if(t == 1){
-               inserir(cd, &n, &m);
+               // inserir(cd, &n, &m);
+               inserir(&cd, &n);
+          }
+          else if(t == 3){
+               for(int c=0; c<n; c++){
+                    printar(cd[c]);
+               }
+          } 
+          else if(t == 4){
+               int opc = 0;
+               do{
+                    printf("Digite o indice da musica a ser mostrada: ");
+                    scanf("%d", &opc);
+               }while(opc < 0 || opc > n - 1);
+               printar(cd[opc]);
           }
      }
 
-     print(cd, &n);
-
+     free(cd);
      return 0;
 }
 
-void printm(){
+void printMenu(){
      printf("\nDigite a ação desejada:\
      \n1 - Inserir uma nova musica ao registro\
-     \n2- Remover uma musica do registro\
-     \n3- Exibir as musicas registradas\
-     \n4- Consultar um registro especifico\
-     \n5- Sair do programa\n > ");
+     \n2 - Remover uma musica do registro\
+     \n3 - Exibir as musicas registradas\
+     \n4 - Consultar um registro especifico\
+     \n5 - Sair do programa\n > ");
 }
 
 // Insere as informacoes em musica e nos structs correspondentes
 // inserir(struct musica <pont das musicas>, int <cap do vetor>, int <qnt de musicas>)
-void inserir(struct musica *cd, int *n, int *m){
-     (*m)++;
-     if(*n < *m){
-          (*n)++;
-          cd = realloc(cd, (*n) * sizeof(struct musica));
-     }
+
+// void inserir(struct musica *cd, int *n, int *m){
+     // (*m)++;
+     // if(*n < *m){
+     //      (*n)++;
+     //      cd = realloc(cd, (*n) * sizeof(struct musica));
+     // }
+void inserir(struct musica **cd, int *n){
+     (*n)++;
+     *cd = realloc(*cd, sizeof(struct musica) * (*n));
 
      getchar();
      printf("\nDigite o nome da musica: ");
-     scanf("%[^\n]s", cd[*m - 1].titulo);
+     // scanf("%[^\n]s", cd[*m - 1].titulo);
+     scanf("%[^\n]s", (*cd)[*n - 1].titulo);
 
-     for(int i=0; i < *m - 1; i++){
-          if(strcmp(cd[i].titulo, cd[*m - 1].titulo) == 0){
-               printf("Musica ja cadastrada\n");
-               (*m)--;
+     // for(int i=0; i < *m - 1; i++){
+     //      if(strcmp(cd[i].titulo, cd[*m - 1].titulo) == 0){
+     //           printf("Musica ja cadastrada!\n");
+     //           (*m)--;
+     //           return;
+     //      }
+     for(int i=0; i < *n - 1; i++){
+          if(strcmp((*cd)[i].titulo, (*cd)[*n - 1].titulo) == 0){
+               printf("Musica ja cadastrada!\n");
+               (*n)--;
                return;
           }
      }
+     if(search(*cd, (*cd)[*n - 1].titulo, n) == -1){
+          printf("Musica ja cadastrada!\n");
+          (*n)--;
+          return;
+     }
+
      getchar();
      printf("Informe o genero musical: ");
-     scanf("%[^\n]s", cd[*n - 1].estilo);
+     scanf("%[^\n]s", (*cd)[*n - 1].estilo);
      getchar();
      printf("Informe a gravadora: ");
-     scanf("%[^\n]s", cd[*n - 1].gravadora);
+     scanf("%[^\n]s", (*cd)[*n - 1].gravadora);
      printf("Informe a data de lancamento (formato dd mm aaaa): ");
-     scanf("%d%d%d", &cd[*n - 1].data.dia, &cd[*n - 1].data.mes, &cd[*n - 1].data.ano);
+     scanf("%d%d%d", &(*cd)[*n - 1].data.dia, &(*cd)[*n - 1].data.mes, &(*cd)[*n - 1].data.ano);
      printf("Informe a duracao da musica (tempo aproximado em minutos): ");
-     scanf("%d", &cd[*n - 1].tempo);
+     scanf("%d", &(*cd)[*n - 1].tempo);
      getchar();
      printf("Informe o nome do(as) compositor(as): ");
-     scanf("%[^\n]s", cd[*n - 1].compositor.nome);
+     scanf("%[^\n]s", (*cd)[*n - 1].compositor.nome);
      getchar();
      printf("Informe a nacionalidade do/da compositor/compositora: ");
-     scanf("%[^\n]s", cd[*n-1].compositor.nacionalidade);
+     scanf("%[^\n]s", (*cd)[*n - 1].compositor.nacionalidade);
 }
 
-void print(struct musica *mus, int *id){
-     (*id)--;
+void printar(struct musica mus){
      printf("\nNome: %s\
      \nGenero: %s\
      \nGravadora: %s\
@@ -97,7 +127,16 @@ void print(struct musica *mus, int *id){
      \nDuracao: %d min\
      \nCompositor(es): %s\
      \nNacionalidade(s): %s\n\
-     ", mus[*id].titulo, mus[*id].estilo, mus[*id].gravadora,
-     mus[*id].data.dia, mus[*id].data.mes, mus[*id].data.ano,
-     mus[*id].tempo, mus[*id].compositor.nome, mus[*id].compositor.nacionalidade);
+     ", mus.titulo, mus.estilo, mus.gravadora,
+     mus.data.dia, mus.data.mes, mus.data.ano,
+     mus.tempo, mus.compositor.nome, mus.compositor.nacionalidade);
+}
+
+int search(struct musica *cd, char *key, int *len){
+     for(int i = 0; i < *len; i++){
+          if(strcmp((*cd).titulo, (*cd).titulo) == 0){
+               return i;
+          }
+     }
+     return -1;
 }

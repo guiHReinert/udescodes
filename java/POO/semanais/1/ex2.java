5f42812317682a0c1796a59bf29ac64fb9854411
Guilherme Hoerning Reinert <guilhermeh.rei@gmail.com>
2025-03-12 20:55:49 -0300

    created the java and POO folders

---
+++ java/POO/semanais/1/ex2.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class ex2{
+    public static void main(String[] args){
+        Scanner escaninho = new Scanner(System.in);
+
+        String[] nomes = new String[3];
+        int[] ages =  new int[3];
+        
+        for(int j=0; j<3; j++){
+            System.out.format("Digite o nome da %da pessoa:\n >> ", j+1);
+            nomes[j] = escaninho.nextLine();
+            System.out.format("Digite a idade de %s:\n >> ", nomes[j]);
+            ages[j] = escaninho.nextInt();
+
+            escaninho.nextLine();
+        }
+
+        System.out.println("");
+        for(int k=0; k<3; k++){
+            System.out.format("%d: %s\nidade: %d\n", k+1, nomes[k], ages[k]);
+        }
+
+    }
+}

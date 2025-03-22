27210acc9d0f1ddfea7a529c921c064e265d802a
Guilherme Hoerning Reinert <guilhermeHReinert@guilhermeh.rei@gmail.com>
2025-03-21 23:58:26 -0300

    placeholder

--- java/POO/projetos/Javali/src/main/Dono.java
+++ java/POO/projetos/Javali/src/main/Dono.java
@@ -6,6 +6,23 @@ public class Dono{
     private String cpf;
 
     public String getNome(){
-        
+        return nome;
+    }
+    public void setNome(String n){
+        nome = n;
+    }
+
+    public String getEndereco(){
+        return endereco;
+    }
+    public void setEndereco(String e){
+        endereco = e;
+    }
+
+    public String getCPF(){
+        return cpf;
+    }
+    public void setCPF(String c){
+        nome = c;
     }
 }

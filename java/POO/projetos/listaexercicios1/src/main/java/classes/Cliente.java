6187517dedb4d413b488362bfcaebd8cdc34178e
Guilherme Hoerning Reinert <guilhermeHReinert@guilhermeh.rei@gmail.com>
2025-03-24 15:11:50 -0300

    created the listaexercicos1 in POO

---
+++ java/POO/projetos/listaexercicios1/src/main/java/classes/Cliente.java
@@ -0,0 +1,10 @@
+package classes;
+
+public class Cliente {
+    private String cpf;
+    private String nome;
+    private Endereco endereco;
+    private String nivel;
+    private float cashback;
+
+}
---
+++ java/POO/projetos/listaexercicios1/src/main/java/classes/Produto.java
@@ -0,0 +1,11 @@
+package classes;
+
+public class Produto {
+    private String nome;
+    private int codigo;
+    private String categoria;
+    private float preco;
+    private float desconto;
+    private String dataProducao;
+    private String dataValidade;
+}

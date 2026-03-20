6532cf21f5a1dac2fcd6849e56cf3d45ccdf72ab
guiHReinert <guilhermeh.rei@gmail.com>
2026-03-20 18:11:22 -0300

    c: I've stressed myself with python once again, but not for the last time it seems.

--- c/EDA2/exercicios/revisaoC/3.c
+++ c/EDA2/exercicios/revisaoC/3.c
@@ -12,6 +12,19 @@ Exemplo 2, dado v = [1,2,0], a saída esperada é 3
 */
 
 int main(){
+    int n;
+    printf("Digite o tamanho de v e seus elementos:\n");
+    scanf("%d", &n);
+    int v[n], num;
+    for(int i=0; i<n; i++){
+        scanf("%d", &num);
+        v[i] = num;
+    }
+
+    int m[n], menor = v[0];
+    for(int i=0; i<n; i++){
+
+    }
 
     return 0;
 }

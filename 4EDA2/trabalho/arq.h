5d997e76aef70d504d6a57e3f0ef567b0db3762d
guilhermeHReinert <guilhermeh.rei@gmail.com>
2026-05-29 15:46:48 -0300

    c: Started EDA 2 work

---
+++ 4EDA2/trabalho/arq.h
@@ -0,0 +1,29 @@
+#ifndef ARQ_H
+#define ARQ_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/*
+    Definicao dos nodos
+*/
+
+/*
+    Definicao das arvores
+*/
+
+/*
+    Operacoes de insercao
+*/
+
+/*
+    Operacoes de remocao
+*/
+
+/*
+    Funcoes de saida e print
+*/
+
+
+#endif 
